5da236ef670d2fd3c9ffd40dbb3b075b17058b34
nerrak87@gmail.com <nerrak87@gmail.com>
2024-06-25 10:46:57 -0600

    Scaffolded header and added Inter

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import headShot from '../assets/tn.jpg'
 
 const Header = () => {
     return (
-        <header className="text-red-700 font-bold">Header</header>
+        <header className='flex flex-col items-center rounded-md font-inter'>
+            <img src={headShot} className="rounded-t-lg w-screen max-w-[500px] "/>
+            <div className='flex flex-col'>
+                <h1 className=''>Austin Karren</h1>
+                <h2>Software Engineer</h2>
+                <div className="flex">
+                    <button>Email</button>
+                    <button>Linkedn</button>
+                </div>
+            </div>
+
+        </header>
     )
 }
 
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Adjusted paths
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
